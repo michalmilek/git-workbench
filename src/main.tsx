8f25183fa194908c9f2dd8c5d206028ea8d7e85e
Michał Miłek <michal.milek@ideosoftware.com>
2026-05-14 15:27:04 +0200

    feat: add workbench shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+import { App } from "./app/App";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const rootElement = document.getElementById("root");
+
+if (rootElement === null) {
+  throw new Error("Root element #root was not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
